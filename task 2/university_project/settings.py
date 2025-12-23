d352360543a67f1e08a1d86a21a1dc583b5582ab
gecilind <Lind.Geci@Boreksolutions.de>
2025-12-23 09:56:56 +0100

    Update DEBUG and ALLOWED_HOSTS for production
    Set DEBUG to False and specified 'lindgeci.pythonanywhere.com' in ALLOWED_HOSTS to prepare the Django project for deployment.

--- task 2/university_project/settings.py
+++ task 2/university_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8qw#@-h^!-(*5)y-a)+v0fm04rahn1kpta14mmj*4_@6(1i14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lindgeci.pythonanywhere.com']
 
 
 # Application definition
